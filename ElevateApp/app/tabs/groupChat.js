e9a66f09df56b784dd83413a544f10694053dd54
Riley Pittman <pittmanriley7@gmail.com>
2024-12-02 14:22:07 -0800

    created the chat page

--- ElevateApp/app/tabs/groupChat.js
+++ ElevateApp/app/tabs/groupChat.js
@@ -1,47 +1,173 @@
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
   Image,
   Dimensions,
-  TouchableOpacity,
 } from "react-native";
-
+import React, { useState, useEffect } from "react";
 import { useRouter } from "expo-router";
 import Theme from "@/assets/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import Entypo from "@expo/vector-icons/Entypo";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
+// let res = "";
 
 export default function Chat() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  async function handleSend() {
+    if (message.trim() !== "") {
+      const newMessage = {
+        id: messages.length.toString(),
+        text: message,
+      };
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      setMessage("");
+    }
+  }
+
+  const router = useRouter();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.topNav}>
-        <Text style={styles.title}>Chat</Text>
-        <TouchableOpacity onPress={() => alert("not implemented yet!")}>
-          <Entypo style={styles.chat} name="cog" size={36} color="black" />
+    <KeyboardAvoidingView
+      style={styles.container}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 0}
+    >
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+        <Ionicons name="chevron-back" size={30} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.title}>Group Chat</Text>
+      <FlatList
+        data={messages.slice().reverse()}
+        renderItem={({ item }) => (
+          <View
+            style={[
+              styles.messageRow,
+              {
+                justifyContent: "flex-end",
+              },
+            ]}
+          >
+            <View
+              style={[
+                styles.messageContainer,
+                { alignSelf: "flex-end" },
+                {
+                  backgroundColor: Theme.colors.buttonBlue,
+                },
+              ]}
+            >
+              <Text style={[styles.messageText, { color: "black" }]}>
+                {item.text}
+              </Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        inverted // To display the newest messages at the bottom
+      />
+      <View style={[styles.messageRow, { justifyContent: "flex-start" }]}>
+        <Image
+          // source={require("./../images/mingle.png")}
+          style={styles.profileImage}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          multiline
+          placeholder="Say something to your group..."
+          value={message}
+          onChangeText={setMessage}
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Ionicons
+            name="arrow-up"
+            size={Theme.sizes.iconMedium}
+            color="black"
+          />
         </TouchableOpacity>
       </View>
-      <View style={styles.header}></View>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {},
-  topNav: {
-    marginVertical: windowWidth * 0.1,
-    marginHorizontal: windowWidth * 0.07,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+  container: {
+    flex: 1,
+    backgroundColor: Theme.colors.backgroundPrimary,
+  },
+  backButton: {
+    position: "absolute",
+    top: 75,
+    left: 20,
+    zIndex: 1,
   },
   title: {
-    fontWeight: "600",
-    fontSize: 32,
+    textAlign: "center",
+    marginTop: windowHeight * 0.1,
+    fontSize: Theme.sizes.titleText,
+    fontWeight: "bold",
   },
-  chat: {},
+  messageContainer: {
+    padding: 10,
+    borderRadius: 15,
+    marginVertical: 5,
+    marginHorizontal: 10,
+    maxWidth: "70%",
+  },
+  messageRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    maxWidth: "100%", // Ensure the messages and images are contained within the screen width
+  },
+  messageText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  profileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginHorizontal: 5,
+  },
+  sendButton: {
+    marginLeft: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: Theme.colors.buttonBlue,
+    borderRadius: 20,
+  },
+  sendButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  sent: {
+    alignSelf: "flex-end",
+    backgroundColor: "#b3d9ff",
+  },
 });
