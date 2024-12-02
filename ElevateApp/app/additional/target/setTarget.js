76b334900508e8c12fe215689898ad181299cebf
cjindart <cjindart@stanford.edu>
2024-12-02 14:17:41 -0800

    fixed routing from setTarget

--- ElevateApp/app/additional/target/setTarget.js
+++ ElevateApp/app/additional/target/setTarget.js
@@ -57,7 +57,7 @@ export default function SetTarget() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.back} onPress={() => router.back()}>
+      <TouchableOpacity style={styles.back} onPress={() => router.push("/tabs/personalHome")}>
         <Ionicons name="chevron-back" size={30} color="black" />
       </TouchableOpacity>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
